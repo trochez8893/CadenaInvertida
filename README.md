e493f64800b61ccbc93adeaf24bcac0acdccdfd2
trochez8893 <jairotrochezcoto@gmail.com>
2022-08-01 18:11:31 -0600

    cambio codigo

--- README.md
+++
@@ -1,3 +0,0 @@
-### Ejercicio de operador aritmetico
-
-*Realizado en el lenguaje de JAVA utilizando el IDE de Netbeans
--- src/cadenainvertida/CadenaInvertida.java
+++ src/cadenainvertida/CadenaInvertida.java
@@ -7,16 +7,21 @@ public class CadenaInvertida {
     public static void main(String[] args) {
      
 
-        Scanner teclado=new Scanner (System.in);
+        Scanner token=new Scanner (System.in);
         System.out.print(" Ingrese dato a invertir: ");
-        String texto=teclado.nextLine();
-        
-        String invertir="";
-        for(int indice=texto.length()-1;indice >=0; indice--){
-            invertir +=texto.charAt(indice);
+        String texto=token.nextLine().toUpperCase();
+        String aux1="";
+        char[] aux2=new char[texto.length()];
+      for(int i=texto.length()-1;i>=0; i--){
+            char c=texto.charAt(i);
+            if(c>=65&&c<=90){
+                aux1+=c;
+            }else {
+                aux2[i]=c;
+            }
         }
-        System.out.println("Dato original ingresado es:" + texto);
+        
          System.out.println("----------------------------------");
-         System.out.println("Dato invertido  es: " + invertir);
+         System.out.println( "Su dato invertido es:" +aux1+String.valueOf(aux2));
         
     } }
