5f2f658dfdcdc7bf7dca429b986e0f374e54c121
trochez8893 <jairotrochezcoto@gmail.com>
2022-08-03 18:51:29 -0600

    terminado

--- src/cadenainvertida/CadenaInvertida.java
+++ src/cadenainvertida/CadenaInvertida.java
@@ -9,19 +9,20 @@ public class CadenaInvertida {
 
         Scanner token=new Scanner (System.in);
         System.out.print(" Ingrese dato a invertir: ");
-        String texto=token.nextLine().toUpperCase();
-        String aux1="";
-        char[] aux2=new char[texto.length()];
+        String texto=token.nextLine();
+        String dato1="";
+        char[] dato2=new char[texto.length()];
       for(int i=texto.length()-1;i>=0; i--){
             char c=texto.charAt(i);
-            if(c>=65&&c<=90){
-                aux1+=c;
+            if(c>=97&&c<=122){
+          
+                dato1+=c;
             }else {
-                aux2[i]=c;
+                dato2[i]=c;
             }
         }
-        
+      
          System.out.println("----------------------------------");
-         System.out.println( "Su dato invertido es:" +aux1+String.valueOf(aux2));
-        
+         System.out.println( "Su dato invertido es:" +dato1+String.valueOf(dato2));
+         
     } }
